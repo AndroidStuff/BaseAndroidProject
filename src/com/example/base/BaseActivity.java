30706887a0f397c9cc83eee44fba252aae23827d
Karthik Sirasanagandla <sirkarthik@gmail.com>
2016-03-30 13:04:16 +0530

    Added ProgressDialog feature to BaseActivity

--- src/com/example/base/BaseActivity.java
+++ src/com/example/base/BaseActivity.java
@@ -1,7 +1,36 @@
 package com.example.base;
 
 import android.app.Activity;
+import android.app.ProgressDialog;
+import android.util.Log;
 
 public abstract class BaseActivity extends Activity {
+
+	private ProgressDialog progressDialog;
+	private boolean destroyed = false;
+
+	@Override
+	protected void onDestroy() {
+		super.onDestroy();
+		Log.d(getClass().getSimpleName(), "In onDestroy()");
+		destroyed = true;
+	}
+
+	protected void showProgressDialog(CharSequence message) {
+		Log.d(getClass().getSimpleName(), "In showProgressDialog(..)");
+		if (progressDialog == null) {
+			progressDialog = new ProgressDialog(this);
+			progressDialog.setIndeterminate(true);
+		}
+		progressDialog.setMessage(message);
+		progressDialog.show();
+	}
+
+	protected void dismissProgressDialog() {
+		Log.d(getClass().getSimpleName(), "In dismissProgressDialog()");
+		if (progressDialog != null && !destroyed) {
+			progressDialog.dismiss();
+		}
+	}
 
 }
